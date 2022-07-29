6bbc0aba068a2d6ec4ae2b61236d06f27cd4b6da
LucasEzequielGiordano <lucasgiordano2219@gmail.com>
2022-07-28 23:23:50 -0300

    feat: added pug

---
+++ pug/src/routes/products.router.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import ProductManager from "../managers/productManager.js";
+import { uploder } from "../utils.js";
+
+const router = Router();
+const productService = new ProductManager();
+
+router.post("/", uploder.single("img"), async (req, res) => {
+  const { name, price } = req.body;
+
+  if (!req.file)
+    return res
+      .status(500)
+      .send({ status: "error", error: "error while loading data" });
+  if (!name || !price)
+    return res
+      .status(400)
+      .send({ status: "error", error: "Incomplete values" });
+  let product = {
+    name,
+    price,
+    img: req.file.filename,
+  };
+  await productService.save(product);
+  res.send({ status: "success", message: "Product added" });
+});
+
+export default router;
